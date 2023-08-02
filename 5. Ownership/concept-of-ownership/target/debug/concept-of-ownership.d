8dee936eaf506e1a3f88e4c8abb01f24ac706e6a
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-08-02 16:40:07 +0530

    concep-of-ownership

---
+++ 5. Ownership/concept-of-ownership/target/debug/concept-of-ownership.d
@@ -0,0 +1,1 @@
+E:\Rust\Getting\ started\5.\ Ownership\concept-of-ownership\target\debug\concept-of-ownership.exe: E:\Rust\Getting\ started\5.\ Ownership\concept-of-ownership\src\main.rs
---
+++ 5. Ownership/concept-of-ownership/target/debug/deps/concept_of_ownership.d
@@ -0,0 +1,5 @@
+E:\Rust\Getting started\5. Ownership\concept-of-ownership\target\debug\deps\concept_of_ownership.exe: src\main.rs
+
+E:\Rust\Getting started\5. Ownership\concept-of-ownership\target\debug\deps\concept_of_ownership.d: src\main.rs
+
+src\main.rs:
