9af31d83d603e8a8f829292cadc1b9746a32ca05
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-08-02 14:34:49 +0530

    chapter 1,2,3

---
+++ 1. Hello Rust/1-Hello-Rust.rs
@@ -0,0 +1,5 @@
+fn main(){
+    println!("HELLO RUST");
+    println!("Rust is an ahead-of-time compiled language, meaning you can compile a program and give the executable to someone else, and they can run it even without having Rust installed.");
+    println!(" use : rustc FILENAME for compilation");
+}
